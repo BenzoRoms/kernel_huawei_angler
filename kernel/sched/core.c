a2aff116c2cf772fa420ee32ec47bb2cd44b5f3b
Peter Zijlstra <peterz@infradead.org>
2014-01-16 17:54:25 +0100

    sched: Fix __sched_setscheduler() nice test
    With the introduction of sched_attr::sched_nice we need to check
    if we've got permission to actually change the nice value.
    
    Daniel found that can_nice() would always fail; and upon
    inspection it turns out that can_nice() only tests to see if we
    can lower the nice value, but it doesn't validate if we're
    lowering or not.
    
    Therefore amend the test to only call can_nice() when we lower
    the nice value.
    
    Reported-and-Tested-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: raistlin@linux.it
    Cc: juri.lelli@gmail.com
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Fixes: d50dde5a10f3 ("sched: Add new scheduler syscalls to support an extended scheduling parameters ABI")
    Link: http://lkml.kernel.org/r/20140116165425.GA9481@laptop.programming.kicks-ass.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5943,7 +5943,8 @@ recheck:
 	 */
 	if (user && !capable(CAP_SYS_NICE)) {
 		if (fair_policy(policy)) {
-			if (!can_nice(p, attr->sched_nice))
+			if (attr->sched_nice < TASK_NICE(p) &&
+			    !can_nice(p, attr->sched_nice))
 				return -EPERM;
 		}
 
