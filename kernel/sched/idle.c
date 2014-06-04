cec7ac8f6cfba9d83151b5b7b2699f3d2cd5e6f9
Andy Lutomirski <luto@amacapital.net>
2014-06-04 10:31:16 -0700

    sched/idle: Clear polling before descheduling the idle thread
    Currently, the only real guarantee provided by the polling bit is
    that, if you hold rq->lock and the polling bit is set, then you can
    set need_resched to force a reschedule.
    
    The only reason the lock is needed is that the idle thread might not
    be running at all when setting its need_resched bit, and rq->lock
    keeps it pinned.
    
    This is easy to fix: just clear the polling bit before scheduling.
    Now the idle thread's polling bit is only ever set when
    rq->curr == rq->idle.
    
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: nicolas.pitre@linaro.org
    Cc: daniel.lezcano@linaro.org
    Cc: umgwanakikbuti@gmail.com
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-kernel@vger.kernel.org
    Link: http://lkml.kernel.org/r/b2059fcb4c613d520cb503b6fad6e47033c7c203.1401902905.git.luto@amacapital.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -80,7 +80,8 @@ static inline int cpu_idle_poll(void)
 	rcu_idle_enter();
 	trace_cpu_idle_rcuidle(0, smp_processor_id());
 	local_irq_enable();
-	while (!tif_need_resched())
+	while (!tif_need_resched() && (cpu_idle_force_poll ||
+		__get_cpu_var(idle_force_poll)))
 		cpu_relax();
 	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, smp_processor_id());
 	rcu_idle_exit();
@@ -271,7 +272,9 @@ static void cpu_idle_loop(void)
 			 * know that the IPI is going to arrive right
 			 * away
 			 */
-			if (cpu_idle_force_poll || tick_check_broadcast_expired())
+			if (cpu_idle_force_poll ||
+			    tick_check_broadcast_expired() ||
+			    __get_cpu_var(idle_force_poll))
 				cpu_idle_poll();
 			else
 				cpuidle_idle_call();
