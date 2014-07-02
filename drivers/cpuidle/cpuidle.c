06754d20e94bac31d4e9e953a7a1ec4b24b97db3
Sandeep Tripathy <sandeep.tripathy@linaro.org>
2014-07-02 15:00:58 +0530

    cpuidle: move idle traces to cpuidle_enter_state()
    idle_exit event is the first event after a core exits
    idle state. So this should be traced before local irq
    is ebabled. Likewise idle_entry is the last event before
    a core enters idle state. This will ease visualising the
    cpu idle state from kernel traces.
    
    Signed-off-by: Sandeep Tripathy <sandeep.tripathy@linaro.org>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    [rjw: Subject, rebase]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -117,7 +117,11 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 
 	struct cpuidle_state *target_state = &drv->states[index];
 
+	trace_cpu_idle_rcuidle(index, dev->cpu);
+
 	entered_state = target_state->enter(dev, drv, index);
+
+	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, dev->cpu);
 
 	if (!cpuidle_state_is_coupled(dev, drv, entered_state))
 		local_irq_enable();
