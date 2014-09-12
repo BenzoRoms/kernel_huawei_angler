993a994510a351a1c6816ecc72ce409e2d5e2a77
Kirill Tkhai <ktkhai@parallels.com>
2014-09-12 17:40:54 +0400

    sched: treewide: Remove set_current_state(TASK_RUNNING) after schedule()
    commit 63333d2ccdf125fb276c351f0cec1d4b3f9219f2
    Author: Christoph Lameter <cl@linux.com>
    Date:   Sun Aug 17 12:30:24 2014 -0500
    
        kernel misc: Replace __get_cpu_var uses
    
        Replace uses of __get_cpu_var for address calculation with this_cpu_ptr.
    
        Cc: akpm@linux-foundation.org
        Signed-off-by: Christoph Lameter <cl@linux.com>
        Signed-off-by: Tejun Heo <tj@kernel.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit f062f8de527ea57963db16f59fb63ff9b77ed0b7
    Author: Kirill Tkhai <ktkhai@parallels.com>
    Date:   Fri Sep 12 17:40:54 2014 +0400
    
        sched, cleanup, treewide: Remove set_current_state(TASK_RUNNING) after schedule()
    
        schedule(), io_schedule() and schedule_timeout() always return
        with TASK_RUNNING state set, so one more setting is unnecessary.
    
        (All places in patch are visible good, only exception is
         kiblnd_scheduler() from:
    
              drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c
    
         Its schedule() is one line above standard 3 lines of unified diff)
    
        No places where set_current_state() is used for mb().
    
        Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
        Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Link: http://lkml.kernel.org/r/1410529254.3569.23.camel@tkhai
        Cc: Alasdair Kergon <agk@redhat.com>
        Cc: Anil Belur <askb23@gmail.com>
        Cc: Arnd Bergmann <arnd@arndb.de>
        Cc: Dave Kleikamp <shaggy@kernel.org>
        Cc: David Airlie <airlied@linux.ie>
        Cc: David Howells <dhowells@redhat.com>
        Cc: Dmitry Eremin <dmitry.eremin@intel.com>
        Cc: Frank Blaschka <blaschka@linux.vnet.ibm.com>
        Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
        Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
        Cc: Helge Deller <deller@gmx.de>
        Cc: Isaac Huang <he.huang@intel.com>
        Cc: James E.J. Bottomley <JBottomley@parallels.com>
        Cc: James E.J. Bottomley <jejb@parisc-linux.org>
        Cc: J. Bruce Fields <bfields@fieldses.org>
        Cc: Jeff Dike <jdike@addtoit.com>
        Cc: Jesper Nilsson <jesper.nilsson@axis.com>
        Cc: Jiri Slaby <jslaby@suse.cz>
        Cc: Laura Abbott <lauraa@codeaurora.org>
        Cc: Liang Zhen <liang.zhen@intel.com>
        Cc: Linus Torvalds <torvalds@linux-foundation.org>
        Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
        Cc: Masaru Nomura <massa.nomura@gmail.com>
        Cc: Michael Opdenacker <michael.opdenacker@free-electrons.com>
        Cc: Mikael Starvik <starvik@axis.com>
        Cc: Mike Snitzer <snitzer@redhat.com>
        Cc: Neil Brown <neilb@suse.de>
        Cc: Oleg Drokin <green@linuxhacker.ru>
        Cc: Peng Tao <bergwolf@gmail.com>
        Cc: Richard Weinberger <richard@nod.at>
        Cc: Robert Love <robert.w.love@intel.com>
        Cc: Steven Rostedt <rostedt@goodmis.org>
        Cc: Trond Myklebust <trond.myklebust@primarydata.com>
        Cc: Ursula Braun <ursula.braun@de.ibm.com>
        Cc: Zi Shen Lim <zlim.lnx@gmail.com>
        Cc: devel@driverdev.osuosl.org
        Cc: dm-devel@redhat.com
        Cc: dri-devel@lists.freedesktop.org
        Cc: fcoe-devel@open-fcoe.org
        Cc: jfs-discussion@lists.sourceforge.net
        Cc: linux390@de.ibm.com
        Cc: linux-afs@lists.infradead.org
        Cc: linux-cris-kernel@axis.com
        Cc: linux-kernel@vger.kernel.org
        Cc: linux-nfs@vger.kernel.org
        Cc: linux-parisc@vger.kernel.org
        Cc: linux-raid@vger.kernel.org
        Cc: linux-s390@vger.kernel.org
        Cc: linux-scsi@vger.kernel.org
        Cc: qla2xxx-upstream@qlogic.com
        Cc: user-mode-linux-devel@lists.sourceforge.net
        Cc: user-mode-linux-user@lists.sourceforge.net
        Signed-off-by: Ingo Molnar <mingo@kernel.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -2834,7 +2834,7 @@ void wake_up_klogd(void)
 	preempt_disable();
 	if (waitqueue_active(&log_wait)) {
 		this_cpu_or(printk_pending, PRINTK_PENDING_WAKEUP);
-		irq_work_queue(&__get_cpu_var(wake_up_klogd_work));
+		irq_work_queue(this_cpu_ptr(&wake_up_klogd_work));
 	}
 	preempt_enable();
 }
@@ -2854,7 +2854,7 @@ int printk_deferred(const char *fmt, ...)
 	va_end(args);
 
 	__this_cpu_or(printk_pending, PRINTK_PENDING_SCHED);
-	irq_work_queue(&__get_cpu_var(wake_up_klogd_work));
+	irq_work_queue(this_cpu_ptr(&wake_up_klogd_work));
 	local_irq_restore(flags);
 
 	return r;
