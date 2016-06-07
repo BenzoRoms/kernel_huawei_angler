10bd5a5331ade3281071e853eb21c86faa8a021d
Suyog Sarda <ssarda@codeaurora.org>
2016-06-07 21:15:42 +0530

    lowmemorykiller: Introduce sysfs node for ALMK and PPR adj threshold
    The grouping of tasks based on oom_score_adj values change from
    one framework to another. This requires corresponding changes in
    the threshold values set for almk and per process reclaim.
    Introduce sysfs nodes to set threshold adj for process reclaim
    and adaptive LMK dynamically.
    
    Change-Id: Ib7565bfd5d2e93aa4ff8fdd20414cac0a0f38bf7
    Signed-off-by: Suyog Sarda <ssarda@codeaurora.org>
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -77,6 +77,8 @@ static unsigned long lowmem_deathpending_timeout;
 
 static atomic_t shift_adj = ATOMIC_INIT(0);
 static short adj_max_shift = 353;
+module_param_named(adj_max_shift, adj_max_shift, short,
+	S_IRUGO | S_IWUSR);
 
 static int vm_pressure_adaptive_start = 85;
 #define VM_PRESSURE_ADAPTIVE_STOP	95
