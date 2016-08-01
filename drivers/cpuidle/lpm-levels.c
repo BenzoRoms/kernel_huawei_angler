a2fe55b79930e45cb64a0dcd6a8438cbafb46762
Maulik Shah <mkshah@codeaurora.org>
2016-08-01 12:11:13 +0530

    drivers: lpm-levels: Allow rpm handshake only with cluster LPM
    CPU spm is programmed to handshake with rpm for single cpu
    devices. If cluster level LPM is not selected even then spm
    handshakes with rpm.
    
    Fix this issue by allowing rpm handshake only if cluster level
    LPM is selected.
    
    CRs-Fixed: 1043098
    Change-Id: I54babda27ecf7a931f844b8c7219a9582221abca
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1001,6 +1001,9 @@ static int cluster_configure(struct lpm_cluster *cluster, int idx,
 
 		do_div(us, NSEC_PER_SEC/SCLK_HZ);
 		msm_mpm_enter_sleep(us, from_idle, cpumask);
+
+		if (cluster->no_saw_devices)
+			msm_spm_set_rpm_hs(true);
 	}
 	cluster->last_level = idx;
 	spin_unlock(&cluster->sync_lock);
@@ -1135,6 +1138,9 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	if (level->notify_rpm) {
 		msm_rpm_exit_sleep();
 		msm_mpm_exit_sleep(from_idle);
+
+		if (cluster->no_saw_devices)
+			msm_spm_set_rpm_hs(false);
 	}
 
 	update_debug_pc_event(CLUSTER_EXIT, cluster->last_level,
