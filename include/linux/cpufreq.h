75d0501180be349def727261070484d3d0172088
mydongistiny <jaysonedson@gmail.com>
2016-11-24 23:27:45 -0800

    Revert "zzmoove: bring in big.LITTLE Edition (bLE) development version"
    This reverts commit 6b100f8ab4b29bcb36db020ac99091fb4ba6c5cd.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -471,9 +471,6 @@ extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
-extern struct cpufreq_governor cpufreq_gov_zzmoove;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
