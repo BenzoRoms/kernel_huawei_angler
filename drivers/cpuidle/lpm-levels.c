66225780db2da4594ce213ce5f982f6b96e73e82
Srinivas Rao L <lsrao@codeaurora.org>
2016-12-29 18:49:54 +0530

    lpm-levels: Do not consider cluster residency in suspend
    If cluster deepest low power mode is disabled in idle path, the
    residencies are recalculated and the max_residency for the shallower
    mode becomes ~0 even if deepest mode is not disabled in suspend path.
    Because of this while the cluster mode is selected during suspend,
    it selects shallower mode instead of deepest mode available.
    
    During suspend ignore the residency to select the cluster level
    low power mode.
    
    Change-Id: I812e33ad45e563b88c478d5d70296d5bd488438f
    Signed-off-by: Srinivas Rao L <lsrao@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -921,8 +921,9 @@ static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 
 		best_level = i;
 
-		if (predicted ? (pred_us <= pwr_params->max_residency)
-			: (from_idle && sleep_us <= pwr_params->max_residency))
+		if (from_idle &&
+			(predicted ? (pred_us <= pwr_params->max_residency)
+			: (sleep_us <= pwr_params->max_residency)))
 			break;
 	}
 
