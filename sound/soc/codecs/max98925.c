c9b4217251e889e33dc61ddc2c08675302c071d4
Joe Maples <joe@frap129.org>
2017-05-05 13:02:45 -0500

    sound: Change paths to match FauxSound
    commit baab852e78a61418ff92a03bd8d29e428038ede1
    Author: Joe Maples <joe@frap129.org>
    Date:   Fri May 5 13:02:45 2017 -0500
    
        sound: Change paths to match FauxSound
    
        Signed-off-by: Joe Maples <joe@frap129.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 2215de3bafb9b2e601bd5d57bd9838d97748a55a
    Author: Adin Kwok <adin.kwok@gmail.com>
    Date:   Sat Oct 1 10:08:40 2016 -0700
    
        ASoC: wcd9330: add high performance mode toggle
    
        * path is /sys/kernel/sound_control_3/highperf_enabled
        * 1 to enable, 0 to disable
        * enabled by default
    
        @nathanchance edits: clean up logic and fix incompatible pointer types warning
    
        Signed-off-by: Adin Kwok <adin.kwok@gmail.com>
        Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
        Signed-off-by: Joe Maples <joe@frap129.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 3e88db3b48ae02cb45bf8c45077dce6fae4ab96d
    Author: flar2 <asegaert@gmail.com>
    Date:   Fri Sep 2 13:56:12 2016 -0400
    
        ASoC: wcd9330: add analog headphone gain and update controls
    
        -disable compander for headphones
        -prevent mixerpath from changing headphone gain
        -change sysfs paths to work with upcoming EX Kernel Manager controls
    
        Signed-off-by: Joe Maples <joe@frap129.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 520b85c9f106f803d57f177aa4294aafc9dfe490
    Author: flar2 <asegaert@gmail.com>
    Date:   Wed Nov 11 23:41:25 2015 -0500
    
        sound control: add speaker gain tunable
    
        Signed-off-by: Joe Maples <joe@frap129.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 0fe5b34f0dfc4e3c718cceb7ed676e804a316c14
    Author: Pavel <nadyaivanova14@gmail.com>
    Date:   Fri Jan 23 14:34:23 2015 +0100
    
        sound/soc/codecs: fix speaker gain and mic gain for the Note 4
    
        Signed-off-by: Pafcholini <pafcholini@gmail.com>
        Signed-off-by: flar2 <asegaert@gmail.com>
        Signed-off-by: Joe Maples <joe@frap129.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit f1ae8dfc4ae2240492f71c20c81aa750c2d5c2f5
    Author: Pavel <nadyaivanova14@gmail.com>
    Date:   Tue Nov 10 09:56:52 2015 -0500
    
        sound/soc/codecs: Faux Sound control for WCD9330 TomTom codec driver
    
        First implementation:
        Faux Sound control adapted for the wcd9330 driver.
    
        Signed-off-by: flar2 <asegaert@gmail.com>
        Signed-off-by: Joe Maples <joe@frap129.org>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 183c80be18edcaeb7e42b76ba3c65a0b2d603081
    Author: mydongistiny <jaysonedson@gmail.com>
    Date:   Sun May 7 17:25:21 2017 -0700
    
        Revert "drivers: sound: add basic sound control, heaphones, mic, camera mic"
    
        This reverts commit 237688a.
    
    commit a78637bae6c57e17a8d8c014c80680beac3e4b20
    Author: mydongistiny <jaysonedson@gmail.com>
    Date:   Sun May 7 17:25:11 2017 -0700
    
        Revert "sound: soc: add in the hipref audio sysfs for francos"
    
        This reverts commit 4eb6097.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- sound/soc/codecs/max98925.c
+++ sound/soc/codecs/max98925.c
@@ -45,9 +45,10 @@ static uint8_t *payload = (uint8_t *)&param_array[100];
 
 static DEFINE_MUTEX(dsm_lock);
 
-#define DEFAULT_SPEAKER_GAIN 20
-int speaker_l_gain = DEFAULT_SPEAKER_GAIN;
-int speaker_r_gain = DEFAULT_SPEAKER_GAIN;
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+int speaker_gain_lval = 20;
+int speaker_gain_rval = 20;
+#endif
 
 static struct reg_default max98925_reg[] = {
 	{ 0x00, 0x00 }, /* Battery Voltage Data */
@@ -378,16 +379,6 @@ static const unsigned int max98925_spk_tlv[] = {
 	1, 31, TLV_DB_SCALE_ITEM(-600, 100, 0),
 };
 
-void update_speakers_l_gain(int vol_boost)
-{
-	speaker_l_gain = DEFAULT_SPEAKER_GAIN + vol_boost;
-}
-
-void update_speakers_r_gain(int vol_boost)
-{
-	speaker_r_gain = DEFAULT_SPEAKER_GAIN + vol_boost;
-}
-
 static int max98925_left_gain_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
@@ -412,8 +403,10 @@ static int max98925_left_gain_put(struct snd_kcontrol *kcontrol,
 	struct max98925_priv *max98925 = snd_soc_codec_get_drvdata(codec);
 	unsigned int sel = ucontrol->value.integer.value[0];
 
-	if (sel && speaker_l_gain != DEFAULT_SPEAKER_GAIN)
-		sel = speaker_l_gain;
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+	if (sel == 20)
+		sel = speaker_gain_lval;
+#endif
 
 	if (sel < ((1 << M98925_SPK_GAIN_WIDTH) - 1)) {
 		regmap_update_bits(max98925->regmapL, MAX98925_R02D_GAIN,
@@ -454,8 +447,10 @@ static int max98925_right_gain_put(struct snd_kcontrol *kcontrol,
 	struct max98925_priv *max98925 = snd_soc_codec_get_drvdata(codec);
 	unsigned int sel = ucontrol->value.integer.value[0];
 
-	if (sel && speaker_r_gain != DEFAULT_SPEAKER_GAIN)
-                sel = speaker_r_gain;
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+	if (sel == 20)
+		sel = speaker_gain_rval;
+#endif
 
 	if (sel < ((1 << M98925_SPK_GAIN_WIDTH) - 1)) {
 		regmap_update_bits(max98925->regmapR, MAX98925_R02D_GAIN,
--- sound/soc/codecs/wcd9330.c
+++ sound/soc/codecs/wcd9330.c
@@ -34,26 +34,11 @@
 #include <linux/pm_runtime.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
-#include <linux/sound_control.h>
 #include "wcd9330.h"
 #include "wcd9xxx-resmgr.h"
 #include "wcd9xxx-common.h"
 #include "wcdcal-hwdep.h"
 #include "wcd_cpe_core.h"
-
-struct sound_control {
-	struct snd_soc_codec *snd_control_codec;
-	int default_hp_value;
-	int default_mic_value;
-	int default_camera_mic_value;
-	bool hp_lock;
-	bool mic_lock;
-	bool camera_mic_lock;
-} soundcontrol = {
-	.hp_lock = false,
-	.mic_lock = false,
-	.camera_mic_lock = false,
-};
 
 enum {
 	VI_SENSE_1,
@@ -102,7 +87,7 @@ MODULE_PARM_DESC(cpe_debug_mode, "boot cpe in debug mode");
 
 static atomic_t kp_tomtom_priv;
 
-static int high_perf_mode;
+int high_perf_mode = 1;
 module_param(high_perf_mode, int,
 			S_IRUGO | S_IWUSR | S_IWGRP);
 MODULE_PARM_DESC(high_perf_mode, "enable/disable class AB config for hph");
@@ -1020,6 +1005,9 @@ static int tomtom_set_compander(struct snd_kcontrol *kcontrol,
 		    kcontrol->private_value)->shift;
 	int value = ucontrol->value.integer.value[0];
 
+	if (comp == COMPANDER_1)
+		value = 0;
+
 	pr_debug("%s: Compander %d enable current %d, new %d\n",
 		 __func__, comp, tomtom->comp_enabled[comp], value);
 	tomtom->comp_enabled[comp] = value;
@@ -1765,9 +1753,9 @@ static const struct snd_kcontrol_new tomtom_1_x_analog_gain_controls[] = {
 	SOC_ENUM_EXT("EAR PA Gain", tomtom_1_x_ear_pa_gain_enum,
 		tomtom_pa_gain_get, tomtom_pa_gain_put),
 
-	SOC_SINGLE_TLV("HPHL Volume", TOMTOM_A_RX_HPH_L_GAIN, 0, 20, 1,
+	SOC_SINGLE_TLV("HPH_L Volume", TOMTOM_A_RX_HPH_L_GAIN, 0, 20, 1,
 		line_gain),
-	SOC_SINGLE_TLV("HPHR Volume", TOMTOM_A_RX_HPH_R_GAIN, 0, 20, 1,
+	SOC_SINGLE_TLV("HPH_R Volume", TOMTOM_A_RX_HPH_R_GAIN, 0, 20, 1,
 		line_gain),
 
 	SOC_SINGLE_TLV("LINEOUT1 Volume", TOMTOM_A_RX_LINE_1_GAIN, 0, 20, 1,
@@ -5294,33 +5282,12 @@ static int tomtom_volatile(struct snd_soc_codec *ssc, unsigned int reg)
 	return 0;
 }
 
-static int reg_access(unsigned int reg)
-{
-	int ret = 1;
-
-	switch (reg) {
-		case TOMTOM_A_CDC_RX1_VOL_CTL_B2_CTL:
-		case TOMTOM_A_CDC_RX2_VOL_CTL_B2_CTL:
-			if (soundcontrol.hp_lock)
-                                ret = 0;
-                        break;
-		case TOMTOM_A_CDC_TX6_VOL_CTL_GAIN:
-			if (soundcontrol.mic_lock)
-				ret = 0;
-			break;
-		case TOMTOM_A_CDC_TX4_VOL_CTL_GAIN:
-			if (soundcontrol.camera_mic_lock)
-				ret = 0;
-			break;
-	}
-
-	return ret;
-}
-
-static int tomtom_write(struct snd_soc_codec *codec, unsigned int reg,
+#ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL
+static
+#endif
+int tomtom_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
 {
-	int val;
 	int ret;
 	struct wcd9xxx *wcd9xxx = codec->control_data;
 	struct tomtom_priv *tomtom_p = snd_soc_codec_get_drvdata(codec);
@@ -5340,17 +5307,17 @@ static int tomtom_write(struct snd_soc_codec *codec, unsigned int reg,
 	if (unlikely(test_bit(BUS_DOWN, &tomtom_p->status_mask))) {
 		dev_err(codec->dev, "write 0x%02x while offline\n", reg);
 		return -ENODEV;
-	} else {
-		if (!reg_access(reg))
-			val = wcd9xxx_reg_read(&wcd9xxx->core_res, reg);
-		else
-			val = value;
-
-		return wcd9xxx_reg_write(&wcd9xxx->core_res, reg, val);
-	}
+	} else
+		return wcd9xxx_reg_write(&wcd9xxx->core_res, reg, value);
 }
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+EXPORT_SYMBOL(tomtom_write);
+#endif
 
-static unsigned int tomtom_read(struct snd_soc_codec *codec,
+#ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL 
+static
+#endif
+unsigned int tomtom_read(struct snd_soc_codec *codec,
 				unsigned int reg)
 {
 	unsigned int val;
@@ -5382,6 +5349,9 @@ static unsigned int tomtom_read(struct snd_soc_codec *codec,
 		return val;
 	}
 }
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+EXPORT_SYMBOL(tomtom_read);
+#endif
 
 static int tomtom_startup(struct snd_pcm_substream *substream,
 		struct snd_soc_dai *dai)
@@ -8778,62 +8748,10 @@ static int tomtom_cpe_initialize(struct snd_soc_codec *codec)
 	return 0;
 }
 
-void update_headphones_volume_boost(unsigned int vol_boost)
-{
-	int default_val = soundcontrol.default_hp_value;
-	int boosted_val = default_val + vol_boost;
-
-	pr_info("Sound Control: Headphones default value %d\n", default_val);
-
-	soundcontrol.hp_lock = false;
-	tomtom_write(soundcontrol.snd_control_codec,
-		TOMTOM_A_CDC_RX1_VOL_CTL_B2_CTL, boosted_val);
-	tomtom_write(soundcontrol.snd_control_codec,
-		TOMTOM_A_CDC_RX2_VOL_CTL_B2_CTL, boosted_val);
-	soundcontrol.hp_lock = true;
-
-	pr_info("Sound Control: Boosted Headphones RX1 value %d\n",
-		tomtom_read(soundcontrol.snd_control_codec,
-		TOMTOM_A_CDC_RX1_VOL_CTL_B2_CTL));
-
-	pr_info("Sound Control: Boosted Headphones RX2 value %d\n",
-		tomtom_read(soundcontrol.snd_control_codec,
-		TOMTOM_A_CDC_RX2_VOL_CTL_B2_CTL));
-}
-
-void update_mic_gain(unsigned int vol_boost)
-{
-	int default_val = soundcontrol.default_mic_value;
-	int boosted_val = default_val + vol_boost;
-
-	pr_info("Sound Control: Mic default value %d\n", default_val);
-
-	soundcontrol.mic_lock = false;
-	tomtom_write(soundcontrol.snd_control_codec,
-		TOMTOM_A_CDC_TX6_VOL_CTL_GAIN, boosted_val);
-	soundcontrol.mic_lock = true;
-
-	pr_info("Sound Control: Boosted Mic value %d\n",
-		tomtom_read(soundcontrol.snd_control_codec,
-		TOMTOM_A_CDC_TX6_VOL_CTL_GAIN));
-}
-
-void update_camera_mic_gain(unsigned int vol_boost)
-{
-        int default_val = soundcontrol.default_camera_mic_value;
-        int boosted_val = default_val + vol_boost;
-
-	pr_info("Sound Control: Camera mic default value %d\n", default_val);
-
-        soundcontrol.camera_mic_lock = false;
-        	tomtom_write(soundcontrol.snd_control_codec,
-                TOMTOM_A_CDC_TX4_VOL_CTL_GAIN, boosted_val);
-        soundcontrol.camera_mic_lock = true;
-
-        pr_info("Sound Control: Boosted Camera mic value %d\n",
-                tomtom_read(soundcontrol.snd_control_codec,
-                TOMTOM_A_CDC_TX4_VOL_CTL_GAIN));
-}
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+struct snd_soc_codec *fauxsound_codec_ptr;
+EXPORT_SYMBOL(fauxsound_codec_ptr);
+#endif
 
 static int tomtom_codec_probe(struct snd_soc_codec *codec)
 {
@@ -8847,7 +8765,10 @@ static int tomtom_codec_probe(struct snd_soc_codec *codec)
 	void *ptr = NULL;
 	struct wcd9xxx_core_resource *core_res;
 
-	soundcontrol.snd_control_codec = codec;
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+	pr_info("tomtom codec probe...\n");
+	fauxsound_codec_ptr = codec;
+#endif
 
 	codec->control_data = dev_get_drvdata(codec->dev->parent);
 	control = codec->control_data;
@@ -9022,17 +8943,6 @@ static int tomtom_codec_probe(struct snd_soc_codec *codec)
 		/* Do not fail probe if CPE failed */
 		ret = 0;
 	}
-
-	/*
-	 * Get the default values during probe
-	 */
-	soundcontrol.default_hp_value = tomtom_read(codec,
-		TOMTOM_A_CDC_RX1_VOL_CTL_B2_CTL);
-	soundcontrol.default_mic_value = tomtom_read(codec,
-		TOMTOM_A_CDC_TX6_VOL_CTL_GAIN);
-	soundcontrol.default_camera_mic_value = tomtom_read(codec,
-                TOMTOM_A_CDC_TX4_VOL_CTL_GAIN);
-
 	return ret;
 
 err_pdata:
